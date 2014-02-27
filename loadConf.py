ca251812bdf7016e5a8463f9a14ba6429f1fb482
sidya82 <quentin.rouland.82@gmail.com>
2014-02-27 19:58:09 +0100

    nouveau fichier: loadConf.py 	nouveau fichier: login.py

---
+++ loadConf.py
@@ -0,0 +1,16 @@
+class LoadConf(object):
+    def loadHand(self):
+        with open("fichier/conf", "r") as source :
+            for ligne in source:
+                data = ligne.rstrip('\n\r').split('=')
+                if data[0] in 'handicap' :
+                        source.close()
+                        return data[1]
+            source.close()
+        return 0
+
+    def estAveugle(self):
+        if self.loadHand() == "1" :
+            return True
+        else :
+            return False
---
+++ login.py
@@ -0,0 +1,38 @@
+import time
+import hashlib
+import httplib
+from datetime import datetime
+
+class Login(object):
+    def verifLogin(self,pLog,pMdp):
+        hashMdp = hashlib.sha224(pMdp).hexdigest()
+	print "Hash pass " + hashMdp
+        with open("fichier/autorise", "r") as source :
+            for ligne in source :
+                data = ligne.rstrip('\n\r').split(',')
+                if data[0] in pLog :
+                    if data[1] in hashMdp :
+                        source.close()
+                        return True
+            source.close()
+        return False
+
+    def enregDansLog(self,pLog,pMsg,pIP):
+        with open("fichier/log", "a") as dest :
+            d = datetime.now().strftime("%c")
+            dest.write("%s,%s,%s,%s\n" % (d,pLog,pMsg,pIP))
+
+    def connexion(self,pLog,pMdp):
+        if self.verifLogin(pLog,pMdp) == True :
+            return True
+        else :
+            return False
+
+
+
+
+
+
+
+
+

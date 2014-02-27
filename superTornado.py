57330baf36d2f11500a6fdcfad2d4e5f3afc1ba0
sidya82 <quentin.rouland.82@gmail.com>
2014-02-27 15:43:58 +0100

    nouveau fichier: superTornado.py

---
+++ superTornado.py
@@ -0,0 +1,20 @@
+import tornado.httpserver
+import tornado.ioloop
+
+
+
+
+
+def main(port = 80):
+    ioloop = tornado.ioloop.IOLoop.instance()
+    application = tornado.web.Application([
+    (r"/get/(sysinfo|cpuinfo)", InfoHandler)
+    ])
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(port)
+    try:
+    ioloop.start()
+    except KeyboardInterrupt:
+    pass
+
+

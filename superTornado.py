9c7911b4653bd945fed68f627f1b198869b43ff9
sidya82 <quentin.rouland.82@gmail.com>
2014-02-27 19:56:31 +0100

    modifié:         superTornado.py

--- superTornado.py
+++ superTornado.py
@@ -23,10 +23,10 @@ application = tornado.web.Application([
 if __name__ == "__main__":
     #chargement congig
     confAveug = hand.estAveugle()
-        if confAveug == True:
-            print "->Blind unhabitant system configuration"
-        else :
-            print "->Not blind unhabitant system configuration"
+    if confAveug == True:
+        print "->Blind unhabitant system configuration"
+    else :
+        print "->Not blind unhabitant system configuration"
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(80)
     tornado.ioloop.IOLoop.instance().start()

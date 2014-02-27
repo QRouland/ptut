7dc16456802bb2fc073606f18cd2874a3d9b35cd
sidya82 <quentin.rouland.82@gmail.com>
2014-02-27 17:56:39 +0100

    modifié:         superTornado.py

--- superTornado.py
+++ superTornado.py
@@ -5,16 +5,16 @@ import tornado.httpserver
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
-
-class LoginHandler(tornado.web.RequestHandler):
     def post(self):
-        #iden = self.get_argument("id",'')
-        #mdp = self.get_argument("mdp",'')
-        self.write("Le pseudo est :")
+        iden = self.get_argument("id",'')
+        mdp = self.get_argument("mdp",'')
+        self.write("Le pseudo est :", iden)
+
+
+
 
 application = tornado.web.Application([
     (r"/", MainHandler),
-    (r"/login", LoginHandler),
 ])
 
 if __name__ == "__main__":

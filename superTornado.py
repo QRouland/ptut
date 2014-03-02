e20fd564efe26c4ba94673479a3aaae9fe703ada
sidya82 <quentin.rouland.82@gmail.com>
2014-03-03 00:08:21 +0100

    modifié:         superTornado.py

--- superTornado.py
+++ superTornado.py
@@ -156,18 +156,9 @@ if __name__ == "__main__":
     print "->Loading configuration ... "
     try :
         blind = config.isBlind()
-        ipCamera = config.ipCamera()
-        portCamera = config.portCamera()
-        portServ = config.portServ()
         if blind == "error" :
             raise BlindConfigurationERROR("Failed Load Blind Configuration")
-        if ipCamera == "error" :
-            raise IPCameraConfigurationERROR("Failed Load IP Camera Configuration")
-        if portCamera == "error" :
-            raise PortCameraConfigurationERROR("Failed Load IP Camera Configuration")
-        if portServ == "error" :
-            raise PortServConfigurationERROR("Failed Load Port Server Configuration")
-    except Exception as e :
+    except BlindConfigurationERROR as e :
         print bcolors.FAIL
         print e
         print "Configuration Loading Failed ! Check Configuration File !" + bcolors.ENDC

19d6c72378b2ccf59a8ca57404cef1888fd76950
sidya82 <quentin.rouland.82@gmail.com>
2014-03-02 21:14:50 +0100

    modifié:         superTornado.py 	modifié:         v/video.html

--- superTornado.py
+++ superTornado.py
@@ -121,7 +121,7 @@ class WSocketHandler(BaseHandler,tornado.websocket.WebSocketHandler):
     def send_image(self) :
         try :
             socket.setdefaulttimeout(5)
-            f = urlopen('http://test:a@192.168.0.13/image.jpg?cidx=791836195')
+            f = urlopen('http://test:a@192.168.1.13/image.jpg?cidx=791836195')
             data = f.read()
             encoded = base64.b64encode(data)
             self.write_message(encoded)

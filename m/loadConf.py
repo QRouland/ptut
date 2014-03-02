3c87c399a737ca482fcec75517edf1b339b76aa6
sidya82 <quentin.rouland.82@gmail.com>
2014-03-03 00:57:20 +0100

    modifié:         m/loadConf.py

--- m/loadConf.py
+++ m/loadConf.py
@@ -3,9 +3,9 @@ class LoadConf(object):
         with open("m/fichier/conf", "r") as source :
             for ligne in source:
                 data = ligne.rstrip('\n\r').split('=')
-                    if data[0] == key :
-                            source.close()
-                            return data[1]
+                if data[0] == key :
+                    source.close()
+                    return data[1]
             source.close()
         return "error"
 

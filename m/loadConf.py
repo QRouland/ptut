3f6c1652f8f74d315f6480c1155044538ba595b0
sidya82 <quentin.rouland.82@gmail.com>
2014-03-02 23:38:11 +0100

    Modifications qui seront validées :

--- m/loadConf.py
+++ m/loadConf.py
@@ -23,3 +23,19 @@ class LoadConf(object):
 
     def portServ(self) :
         return self.loadValue("port")
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+    def disable(self):
+        self.HEADER = ''
+        self.OKBLUE = ''
+        self.OKGREEN = ''
+        self.WARNING = ''
+        self.FAIL = ''
+        self.ENDC = ''

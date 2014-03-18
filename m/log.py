041136659c00a668adcdf09703f4523959fc3427
sidya82 <quentin.rouland.82@gmail.com>
2014-03-18 21:35:07 +0100

    modifié:         m/log.py

--- m/log.py
+++ m/log.py
@@ -33,6 +33,11 @@ class SingleLevelFilter(logging.Filter):
         """
         self.passlevel = passlevel
         self.reject = reject
+    def filter(self, record):
+         if self.reject:
+             return (record.levelno != self.passlevel)
+         else:
+             return (record.levelno == self.passlevel)
 
 class Log(object):
     """
